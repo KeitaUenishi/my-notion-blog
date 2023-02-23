1e0c39787b3894f6be3d7b015a42d4fb3384fb55
KeitaUenishi <keita.uenishi@gmail.com>
2023-02-23 16:47:35 +0900

    Provisional commit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,6 @@
 
 import { usePathname } from "next/navigation"
 import Link from 'next/link'
-import { NEXT_PUBLIC_SITE_TITLE } from 'app/server-constants'
 import styles from 'styles/header.module.css'
 
 interface NavItem {
@@ -14,18 +13,14 @@ const Header = () => {
   const pathname = usePathname()
 
   const navItems: NavItem[] = [
-    { label: 'Home', path: '/' },
+    { label: 'Top', path: '/' },
     { label: 'Blog', path: '/blog' },
+    { label: 'Profile', path: '/blog' },
+    { label: 'Contact', path: '/blog' },
   ]
 
   return (
     <header className={styles.header}>
-      <h1>
-        <Link href="/">
-          {NEXT_PUBLIC_SITE_TITLE}
-        </Link>
-      </h1>
-
       <ul>
         {navItems.map(({ label, path }) => (
           <li key={label}>
